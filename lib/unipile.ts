fed01963710c04d167094c18c134bdb1abb85ac3
Sourabh Mishra <sourabhmishra1262@gmail.com>
2025-03-21 17:08:59 +0530

    Update Unipile API endpoint to use new server URL

--- lib/unipile.ts
+++ lib/unipile.ts
@@ -1,7 +1,7 @@
 import { UnipileClient } from "unipile-node-sdk";
 
-const client = new UnipileClient("https://api4.unipile.com:13459", process.env.NEXT_UNIPILE_ACCESS_TOKEN!);
+const client = new UnipileClient("https://api11.unipile.com:14141", process.env.NEXT_UNIPILE_ACCESS_TOKEN!);
 
-export const UNIPINE_BASE_URL = "https://api4.unipile.com:13459";
+export const UNIPINE_BASE_URL = "https://api11.unipile.com:14141";
 
 export default client;
